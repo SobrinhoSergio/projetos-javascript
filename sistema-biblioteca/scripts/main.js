ede15869d889a57318798fbc61db78bcfdc9edce
Sérgio Sobrinho <sobrinhosergio00@gmail.com>
2021-05-04 16:51:40 -0300

    add projeto

---
+++ sistema-biblioteca/scripts/main.js
@@ -0,0 +1,85 @@
+const form = document.getElementById('formulario');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    
+    var titulo = document.getElementById('titulo').value;
+	var descricao = document.getElementById('descricao').value;
+    var autor = document.getElementById('autor').value;
+
+    if(!titulo || !descricao || !autor){
+		
+		alert("Preencha todos os campos!");
+		
+        return false;
+	
+    } 
+
+        var livro = {
+            
+            titulo: titulo,
+            descricao: descricao,
+            autor: autor
+        };
+
+        if(localStorage.getItem('biblioteca') === null){
+            var livros = [];
+            livros.push(livro);
+            localStorage.setItem('biblioteca', JSON.stringify(livros));
+        
+        } 
+        
+        else {
+            var livros = JSON.parse(localStorage.getItem('biblioteca'));
+            livros.push(livro);
+            localStorage.setItem('biblioteca', JSON.stringify(livros));
+        }
+
+        document.getElementById('formulario').reset();
+
+        mostraBiblioteca();
+
+        event.preventDefault();
+
+}
+
+
+function removeLivro(titulo){
+	
+    var biblioteca = JSON.parse(localStorage.getItem('biblioteca'));
+    console.log(biblioteca);
+
+	 for(var i = 0 ; i < biblioteca.length; i++){
+		
+        if(biblioteca[i].titulo == titulo){
+			
+            biblioteca.splice(i, 1);
+		}
+	}
+
+	localStorage.setItem('biblioteca', JSON.stringify(biblioteca));
+
+	mostraBiblioteca();
+}
+
+function mostraBiblioteca(){
+	
+    var livros = JSON.parse(localStorage.getItem('biblioteca'));
+	
+    var bibliotecaResultado = document.getElementById('resultados');
+
+	bibliotecaResultado.innerHTML = '';
+
+	for(var i = 0; i < livros.length; i++){
+		
+        var titulo = livros[i].titulo;
+		var descricao = livros[i].descricao;
+		var autor = livros[i].autor;
+		 
+        bibliotecaResultado.innerHTML += '<tr><td>'+ titulo + '</td>'+
+		 							 	'<td>'+ descricao + '</td>' +
+		 	 						 	'<td>'+ autor + '</td>' +
+		 							 	'<td><button onclick="removeLivro(\''+ titulo +'\')" class="btn btn-danger">Remover</button></td>'+
+		 							    '</tr>';
+	}
+}
